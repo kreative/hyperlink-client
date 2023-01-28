ab70c939e79211a5cd3c89306b4d3d4783a86348
Armaan Gupta <armaan@guppy.im>
2023-01-28 02:45:24 -0600

    feat: added navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,58 +1,48 @@
-const navigation = [
-  { name: "Contact Us", href: "/contact-us" },
-];
+import Link from "next/link";
+import { ArrowRightIcon } from '@heroicons/react/20/solid'
 
 export default function NavbarComponent() {
   return (
-    <header className="bg-indigo-600">
+    <header className="sticky top-0 bg-white bg-opacity-80 backdrop-blur-md z-10">
       <nav className="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
-        <div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
+        <div className="flex w-full items-center justify-between py-6">
           <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Your Company</span>
+            <Link href="/">
+              <span className="sr-only">Kreative Hyperlink</span>
               <img
                 className="h-10 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=white"
+                src="/hyperlink-logo-color.png"
                 alt=""
               />
-            </a>
-            <div className="ml-10 hidden space-x-8 lg:block items-center">
-              {navigation.map((link) => (
-                <a
-                  key={link.name}
-                  href={link.href}
-                  className="text-base font-medium text-white hover:text-indigo-50"
-                >
-                  {link.name}
-                </a>
-              ))}
-            </div>
+            </Link>
           </div>
-          <div className="ml-10 space-x-4">
-            <a
-              href="#"
-              className="inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75"
+          <div className="ml-10 space-x-8">
+            <Link
+              href={"/contact-us"}
+              className="inline-block py-2 text-base font-medium text-black"
             >
-              Sign in
-            </a>
-            <a
-              href="#"
-              className="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50"
+              Contact us
+            </Link>
+            <Link
+              href={"https://support.kreativeusa.com/hyperlink"}
+              className="inline-block py-2 text-base font-medium text-black"
+            >
+              Get support
+            </Link>
+            <Link
+              href={"#"}
+              className="inline-block py-2 text-base font-medium text-black"
             >
               Sign up
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-wrap justify-center gap-x-6 py-4 lg:hidden">
-          {navigation.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="text-base font-medium text-white hover:text-indigo-50"
+            </Link>
+            <Link
+              href={"#"}
+              className="inline-flex rounded-md border border-transparent bg-blue-800 py-2 px-4 text-base font-medium text-white"
             >
-              {link.name}
-            </a>
-          ))}
+              Dashboard
+              <ArrowRightIcon className="ml-2 -mr-0.5 h-6 w-6" aria-hidden="true" />
+            </Link>
+          </div>
         </div>
       </nav>
     </header>
