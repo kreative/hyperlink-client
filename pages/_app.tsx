ea2b68348eb48637030910603fbab510c7abc218
Armaan Gupta <armaan@guppy.im>
2023-01-30 11:35:01 -0600

    feat: added custom craftwork sans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,48 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Inter } from '@next/font/google'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+// custom craftwork sans font loading locally
+const craftworkSans = localFont({
+  src: [
+    {
+      path: "../styles/fonts/CraftworkSans-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/CraftworkSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/CraftworkSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/CraftworkSans-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/CraftworkSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../styles/fonts/CraftworkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-craftworksans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${craftworkSans.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
