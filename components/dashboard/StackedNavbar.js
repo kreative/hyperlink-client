c9daf8d1713311d731a7413c49647ae7f5161f87
Armaan Gupta <armaan@guppy.im>
2023-02-15 21:54:13 -0600

    ref: removed unused imports and variables

--- components/dashboard/StackedNavbar.js
+++ components/dashboard/StackedNavbar.js
@@ -2,15 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useCookies } from "react-cookie";
-
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 
 const userNavigation = [
   // TODO create a MyKreative profile page that takes in a KSN
