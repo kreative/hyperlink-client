18006f0fd3e6f94997d6fb67a2284c7838d5fa2a
Armaan Gupta <armaan@guppy.im>
2023-02-16 19:06:19 -0600

    feat: added header with new hyperlink button

--- pages/dashboard/links.tsx
+++ pages/dashboard/links.tsx
@@ -26,9 +26,21 @@ export default function Dashboard() {
       <div className="py-10">
         <header>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-              Your Hyperlinks
-            </h1>
+            <div className="md:flex md:items-center md:justify-between">
+              <div className="min-w-0 flex-1">
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                  My Hyperlinks
+                </h2>
+              </div>
+              <div className="mt-4 flex md:mt-0 md:ml-4">
+                <button
+                  type="button"
+                  className="ml-3 inline-flex items-center rounded-md border border-transparent bg-hyper-blue-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-hyper-blue-secondary focus:outline-none focus:ring-2 focus:ring-hyper-blue-light focus:ring-offset-2"
+                >
+                  New Hyperlink
+                </button>
+              </div>
+            </div>
           </div>
         </header>
         <main>
