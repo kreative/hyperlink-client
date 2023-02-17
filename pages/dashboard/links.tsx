adbbf606fd567c70afe5662a28233d70292ab43a
Armaan Gupta <armaan@guppy.im>
2023-02-16 20:54:12 -0600

    feat: created new hyperlink functionality

--- pages/dashboard/links.tsx
+++ pages/dashboard/links.tsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
+import { useState } from "react";
 
 import Authenticate from "@/components/Authenticate";
+import StackedNavbar from "@/components/dashboard/StackedNavbar";
+import HyperlinkList from "@/components/dashboard/hyperlinks/HyperlinkList";
+import CreateLink from "@/components/modals/CreateLink";
 
 // any of the permissions that would allow the user to continue using the application
 const appPermissions = ["KREATIVE_HYPERLINK_USER"];
 
-import StackedNavbar from "@/components/dashboard/StackedNavbar";
-import HyperlinkList from "@/components/dashboard/hyperlinks/HyperlinkList";
+export default function LinksDashboard() {
+  // state for create hyperlink modal
+  const [modalState, setModalState] = useState(false);
 
-export default function Dashboard() {
   const navigation = [
     { name: "Dashboard", href: "/dashboard", current: false },
     { name: "Hyperlinks", href: "/dashboard/links", current: true },
@@ -23,6 +27,7 @@ export default function Dashboard() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <StackedNavbar navigation={navigation} />
+      <CreateLink state={modalState} setState={setModalState} />
       <div className="py-10">
         <header>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -36,6 +41,7 @@ export default function Dashboard() {
                 <button
                   type="button"
                   className="ml-3 inline-flex items-center rounded-md border border-transparent bg-hyper-blue-primary px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-hyper-blue-secondary focus:outline-none focus:ring-2 focus:ring-hyper-blue-light focus:ring-offset-2"
+                  onClick={() => setModalState(true)}
                 >
                   New Hyperlink
                 </button>
